daa455431ad2a6bd187c49ac4521027f2c4bfc07
edit <kajalkasat8@gmail.com>
2022-01-09 01:14:06 +0530

    binary search added

---
+++ Basics/src/leetcode/StackUsingArrays.java
@@ -0,0 +1,17 @@
+package leetcode;
+/*
+stack : LIFO
+
+*/
+class Stack
+{
+    
+}
+public class StackUsingArrays
+{
+    public static void main(String[] args) {
+        
+        int arr[] = new int[5];
+
+    }
+}
