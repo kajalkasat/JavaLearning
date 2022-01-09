c952a7eefd3f4171d031cf5470d65252398e42d6
edit <kajalkasat8@gmail.com>
2022-01-09 19:32:47 +0530

    folder structure corrected

--- Basics/src/collectionsDS/Array/Arraylistcollec.java
+++ Basics/src/collectionsDS/Array/Arraylistcollec.java
@@ -1,10 +1,12 @@
-package collectionsDS;
+package collectionsDS.Array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+ 
+import collectionsDS.Pair;
 
 public class Arraylistcollec {
     
